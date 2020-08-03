56f4bedbb42917a5de8d3af2fbc65412ddc42470
atpetrovskii <artemii.petrovskii@outlook.com>
2020-08-03 12:37:00 +0300

    Update word_splitter.rb

--- word_splitter.rb
+++ word_splitter.rb
@@ -1,7 +1,6 @@
 # This class allows you to perform various operations
 # on the words in a string.
 class WordSplitter
-
   include Enumerable
 
   # The string to split into words.
@@ -16,9 +15,8 @@ class WordSplitter
   # Passes each word in the string to a block, one
   # at a time.
   def each
-    string.split(" ").each do |word|
+    string.split(' ').each do |word|
       yield word
     end
   end
-
 end
