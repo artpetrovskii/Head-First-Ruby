d50bb3215bedcb0a0a5d06d958019e2038860c72
atpetrovskii <artemii.petrovskii@outlook.com>
2020-08-03 11:16:28 +0300

    Lesson4

--- employees.rb
+++ employees.rb
@@ -1,26 +1,25 @@
+# Employee class
 class Employee
-
   attr_reader :name
 
   def name=(name)
-    if name == ""
+    if name == ''
       raise "Name can't be blank!"
     end
     @name = name
   end
 
-  def initialize(name = "Anonymous")
+  def initialize(name = 'Anonymous')
     self.name = name
   end
 
   def print_name
     puts "Name: #{name}"
   end
-
 end
 
+# SalariedEmployee class
 class SalariedEmployee < Employee
-
   attr_reader :salary
 
   def salary=(salary)
@@ -30,7 +29,7 @@ class SalariedEmployee < Employee
     @salary = salary
   end
 
-  def initialize(name = "Anonymous", salary = 0.0)
+  def initialize(name = 'Anonymous', salary = 0.0)
     super(name)
     self.salary = salary
   end
@@ -38,20 +37,21 @@ class SalariedEmployee < Employee
   def print_pay_stub
     print_name
     pay_for_period = (salary / 365.0) * 14
-    formatted_pay = format("$%.2f", pay_for_period)
+    formatted_pay = format('$%.2f', pay_for_period)
     puts "Pay This Period: #{formatted_pay}"
   end
-
 end
 
+# HourlyEmployee class
 class HourlyEmployee < Employee
-
   def self.security_guard(name)
     HourlyEmployee.new(name, 19.25, 30)
   end
+
   def self.cashier(name)
     HourlyEmployee.new(name, 12.75, 25)
   end
+
   def self.janitor(name)
     HourlyEmployee.new(name, 10.50, 20)
   end
@@ -72,7 +72,7 @@ class HourlyEmployee < Employee
     @hours_per_week = hours_per_week
   end
 
-  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+  def initialize(name = 'Anonymous', hourly_wage = 0.0, hours_per_week = 0.0)
     super(name)
     self.hourly_wage = hourly_wage
     self.hours_per_week = hours_per_week
@@ -81,16 +81,15 @@ class HourlyEmployee < Employee
   def print_pay_stub
     print_name
     pay_for_period = hourly_wage * hours_per_week * 2
-    formatted_pay = format("$%.2f", pay_for_period)
+    formatted_pay = format('$%.2f', pay_for_period)
     puts "Pay This Period: #{formatted_pay}"
   end
-
 end
 
-jane = SalariedEmployee.new("Jane Doe", 50000)
+jane = SalariedEmployee.new('Jane Doe', 50_000)
 jane.print_pay_stub
 
-angela = HourlyEmployee.security_guard("Angela Matthews")
-ivan = HourlyEmployee.cashier("Ivan Stokes")
+angela = HourlyEmployee.security_guard('Angela Matthews')
+ivan = HourlyEmployee.cashier('Ivan Stokes')
 angela.print_pay_stub
 ivan.print_pay_stub
